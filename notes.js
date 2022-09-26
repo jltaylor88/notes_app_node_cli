c8cb050211971a8253b7528286c2e93f6cc5fb83
Jason Taylor <jasonleetaylor88@gmail.com>
2022-09-26 07:55:07 +0100

    refactor(add note): move the check for file existence inside the getNotes function

--- notes.js
+++ notes.js
@@ -1,20 +1,24 @@
 const fs = require("fs");
 
 const getNotes = () => {
-	const dataBuffer = fs.readFileSync("notes.json");
-	return dataBuffer.toString();
+	let notes = [];
+	if (fs.existsSync("notes.json")) {
+		const dataBuffer = fs.readFileSync("notes.json");
+		const notesJSON = dataBuffer.toString();
+		notes = JSON.parse(notesJSON);
+	}
+	return notes;
 };
 
 const addNote = (title, body) => {
-	let notes = [];
-	if (fs.existsSync("notes.json")) {
-		notes = JSON.parse(getNotes());
-	}
+	const notes = getNotes();
 
 	notes.push({ title, body });
 	const newNotesJSON = JSON.stringify(notes);
 	fs.writeFileSync("notes.json", newNotesJSON);
 };
+
+const removeNote = () => {};
 
 module.exports = { addNote, getNotes };
 
